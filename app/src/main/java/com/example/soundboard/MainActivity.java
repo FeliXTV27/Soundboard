6d61c16202f88026d00586d9c1c2981f9a301f48
Silvan Lendi <silvan.lendi@gmx.ch>
2020-09-08 08:09:50 +0200

    penis

--- app/src/main/java/com/example/soundboard/MainActivity.java
+++ app/src/main/java/com/example/soundboard/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.soundboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button addFolder;
+    private View.OnClickListener awesomeOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            addFolderclicked();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addFolder = (Button) findViewById(R.id.btn_addFolder);
+
+        addFolder.setOnClickListener(awesomeOnClickListener);
     }
+    private void addFolderclicked() {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+    }
+
 }
