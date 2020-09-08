b0554bf4440bd55eab166d778649cc8f19a172cb
Christian Scherrer <christian.scherrer@bwz-rappi.ch>
2020-09-08 11:08:09 +0200

    rec

--- app/src/main/java/com/example/soundboard/MainActivity.java
+++ app/src/main/java/com/example/soundboard/MainActivity.java
@@ -11,10 +11,19 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button addFolder;
+    Button btnRecordActivity;
+
     private View.OnClickListener awesomeOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             addFolderclicked();
+        }
+    };
+
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            btnRecordActivityclicked();
         }
     };
 
@@ -25,9 +34,18 @@ public class MainActivity extends AppCompatActivity {
 
         addFolder = (Button) findViewById(R.id.btn_addFolder);
         addFolder.setOnClickListener(awesomeOnClickListener);
+
+        btnRecordActivity = (Button) findViewById(R.id.btnRecordActivity);
+        btnRecordActivity.setOnClickListener(onClickListener);
     }
+
     private void addFolderclicked() {
         Intent intent = new Intent(MainActivity.this, AddFolder.class);
+        startActivity(intent);
+    }
+
+    private void btnRecordActivityclicked() {
+        Intent intent = new Intent(MainActivity.this, RecordActivity.class);
         startActivity(intent);
     }
 
